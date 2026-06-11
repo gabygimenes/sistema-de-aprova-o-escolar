170f54181b616adf81e3f860e82a0051fc9103ed
gabygimenes <gabriela.gimenes@edu.senai.br>
2026-06-11 11:41:33 -0300

    fix: refatora o codigo

--- atividade1.js
+++ atividade1.js
@@ -1,1 +1,10 @@
-teste
+//Gabriela Gimenes
+
+//🟢 Nível 1 — Básico
+//1. Lista de nomes
+let alunos = ["Pamela","Paloma","Duda","Manu","Let"];
+console.log (alunos);
+console.log (alunos[0]);
+console.log (alunos[4]);
+
+console.table(alunos);

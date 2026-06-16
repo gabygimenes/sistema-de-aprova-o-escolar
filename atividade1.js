5593813938c36cb299dcefee35f139c667c31647
Gabriela Gimenes <gabriela.gimenes@edu.senai.br>
2026-06-16 15:26:00 -0300

    mensagem do commit

--- atividade1.js
+++ atividade1.js
@@ -2,9 +2,14 @@
 
 //🟢 Nível 1 — Básico
 //1. Lista de nomes
+console.log("======Atividade 1========");
+
 let alunos = ["Pamela","Paloma","Duda","Manu","Let"];
 console.log (alunos);
 console.log (alunos[0]);
 console.log (alunos[4]);
 
 console.table(alunos);
+
+//2. sfdgsdfgsdfhgLista de nomes
+console.log("======Atividade 2========");

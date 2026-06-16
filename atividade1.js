0890048571f65ab5c7bae5f6168292f99e73cf7f
Gabriela Gimenes <gabriela.gimenes@edu.senai.br>
2026-06-16 15:46:34 -0300

    atividades.jsedu

--- atividade1.js
+++ atividade1.js
@@ -11,5 +11,110 @@ console.log (alunos[4]);
 
 console.table(alunos);
 
-//2. sfdgsdfgsdfhgLista de nomes
-console.log("======Atividade 2========");
+console.log('======Atividade 2========');
+//Atividade 2. Lista de frutas
+let frutas = ["Maçã", "Uva", "Morango","Manga"];
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
+
+console.table(frutas);
+
+console.log('======Atividade 3========');
+
+//3. Acessando posições
+let cores = ["Verde", "Azul", "Amarelo", "Rosa", "Roxo"];
+console.log(cores[0]);
+console.log(cores[2]);
+console.log(cores[4]);
+
+console.table(cores);
+
+console.log('======Atividade 4========');
+//4. Alterando um valor
+let produtos = ["Pastel", "Suco", "Coxinha"];
+produtos[1] = "Refrigerante";
+console.log(produtos);
+
+console.table(produtos);
+console.log('======Atividade 5========');
+//5. Quantidade de elementos
+let cidades = ["Campinas", "Valinhos", "Piracicaba","Vinhedo","Hortolandia", "Sorocaba"];
+console.log(" A lista possui " + (cidades.length) + " cidades ");
+
+console.table(cidades);
+console.log('======Atividade 6========');
+//Nível 2 — Manipulação de arrays
+let jogos = ["Mario world", "FIFA", "Valorant"];
+jogos.push(" Fortnite")
+console.log(jogos);
+
+console.table(jogos);
+console.log('======Atividade 7========');
+//7. Adicionando no início
+let materias = ["História", "Ed Fsica", " Bioogia"];
+materias. unshift("Química");
+console.log(materias);
+
+console.table(materias);
+console.log('======Atividade 8========');
+//8  Removendo o último item
+let cantina = ["Coxinha", "Coca cola", "Salgado", "Chocolate"];
+cantina.pop();
+console.log(cantina);
+
+console.table(cantina);
+console.log('======Atividade 9========');
+//9. Removendo o primeiro item
+let nomes = ["Maria B", "Letícia", "Pamela", "Emanuelly"];
+nomes.shift();
+console.log(nomes);
+
+console.table(nomes);
+console.log('======Atividade 10========');
+// 10. Lista atualizada
+let produtoss = ["Pastel", "Suco", "Coxinha", "Chocolate"];
+produtoss.push("Pão de Queijo");
+produtoss.shift();
+produtoss[2] = "Esfiha";
+console.log(produtoss);
+console.log(" A lista possui " + (produtoss.length) + " produtos ");
+
+console.table(produtos);
+console.log('======Atividade 11========');
+// Nível 3 — Percorrendo arrays com for
+//11. Mostrando todos os alunos
+let alunos1 = ["Bianca", "Iara", "Ana Laura", "Gabriela"];
+for (let i = 0; i < alunos.length; i++) {
+    console.log(alunos[i]);
+}
+console.table(alunos);
+console.log('======Atividade 12========');
+//12. Lista numerada
+let filmes = ["Missão impossivel", "Estrelas além do tempo", "Homem aranha", "Pantera negra", "A empregada"];
+for (let i = 0; i < filmes.length; i++) {
+    console.log((i + 1) + " . " + filmes[i])
+}
+console.table(alunos);
+console.log('======Atividade 13========');
+//13. Produtos da cantina
+let produtos2 = ['Pastel', 'Suco', 'Coxinha', 'Chocolate'];
+for (let i = 0; i < produtos2.length; i++) {
+    console.log(" Produtos " + (i + 1) + " : " + produtos[i]);
+}
+console.table(produtos);
+console.log('======Atividade 14========');
+//14. Notas dos alunos
+let notas1 = [10,9,8,7,6];
+for (let i = 0;  i < notas1.length; i++){
+    console.log(notas1[i])
+}
+console.table(notas);
+console.log('======Atividade 15========');
+//15. Verificando presença
+let presentes = ["Paloma está presente", "Fofinha está presente", "Isabella está presente", "Gabriela está presente", "Laura está presente"]
+for (let i = 0; i < presentes.length; i++) {
+    console.log(presentes[i])
+}
+console.table(presentes);
